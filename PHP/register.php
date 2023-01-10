37695814ce3d2455529fe24f8f4f1bd81d4e9db7
elangelis <elangelis@yahoo.gr>
2023-01-10 16:26:52 +0200

    main

--- PHP/register.php
+++ PHP/register.php
@@ -67,62 +67,40 @@
                             $sucess=false;
                         }
                         
-                        
+
                         if($sucess=true){
-                            $sql_register= 'CALL User_creation(:reg_user, :reg_pass, :reg_email)';
-                            $statement = $pdo->prepare($sql_register);
-                            $statement->bindParam(':reg_user',$reg_user,PDO::PARAM_STR_CHAR);
-                            $statement->bindParam(':reg_pass',$reg_pass,PDO::PARAM_STR_CHAR);
-                            $statement->bindParam(':reg_email',$reg_pass,PDO::PARAM_STR_CHAR);
-                            $statement->execute();
-                            echo 'Account created sucesfully';
+                            try{
+                                $sql_register= 'CALL User_creation(:reg_user, :reg_pass, :reg_email)';
+                                $statement = $pdo->prepare($sql_register);
+                                $statement->bindParam(':reg_user',$reg_user,PDO::PARAM_STR_CHAR);
+                                $statement->bindParam(':reg_pass',$reg_pass,PDO::PARAM_STR_CHAR);
+                                $statement->bindParam(':reg_email',$reg_pass,PDO::PARAM_STR_CHAR);
+                                $statement->execute();
+
+                                echo 'Account created sucesfully, you will be transfered to Main Page automaticaly';
+                                header("location:C:\\xampp\\htdocs\\webproject2022\\HTML_CSS_JAVASCRIPT\\2.Main_Page\\Main_Page.php");
+
+                            }catch(PDOException $e){
+                                echo $e->getMessage();
+                            }
+                            $_SESSION['message']=array("text"=>"User successfully created.","alert"=>"info");
+                            $conn = null;
+
                         }
-                    }
-                    else{
-                        echo 'Password must contain 1 Capital letter, 1 Number and any of those "@#$%" symbols and more than 8 Characters';
+                        else{
+                            echo 'Password must contain 1 Capital letter, 1 Number and any of those "@#$%" symbols and more than 8 Characters';
+                        }
+                    }else{
+                        echo 'Confirmation Password should match Password';
                     }
                 }else{
-                    echo 'Confirmation Password should match Password';
+                    echo 'Please provice your registration credentials';
+                    echo "
+                    <script>alert('Please fill up the required field!')</script>
+                    <script>window.location = 'registration.php'</script>
+                    ";
                 }
-            }else{
-                echo 'Please provice your registration credentials';
             }
         }
     }
 ?>
-
-<?php
-
-	session_start();
-	require_once 'conn.php';
- 
-	if(ISSET($_POST['register'])){
-		if($_POST['firstname'] != "" || $_POST['username'] != "" || $_POST['password'] != ""){
-			try{
-				$firstname = $_POST['firstname'];
-				$lastname = $_POST['lastname'];
-				$username = $_POST['username'];
-				// md5 encrypted
-				// $password = md5($_POST['password']);
-				$password = $_POST['password'];
-				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				$sql = "INSERT INTO `member` VALUES ('', '$firstname', '$lastname', '$username', '$password')";
-				$conn->exec($sql);
-			}catch(PDOException $e){
-				echo $e->getMessage();
-			}
-			$_SESSION['message']=array("text"=>"User successfully created.","alert"=>"info");
-			$conn = null;
-			header('location:index.php');
-		}else{
-			echo "
-				<script>alert('Please fill up the required field!')</script>
-				<script>window.location = 'registration.php'</script>
-			";
-		}
-	}
-
-?>
-<html>
-
-</html>
