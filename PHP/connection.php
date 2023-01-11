d94ae31eb02e1943b482d5d8f6dbd13ce354a63c
elangelis <73367290+elangelis@users.noreply.github.com>
2023-01-11 22:03:11 +0200

    main

--- PHP/connection.php
+++ PHP/connection.php
@@ -4,20 +4,20 @@
 require_once 'config.php';
 class Connection
 {
-	public static function make($db_host, $db_name, $db_user, $db_pass)
+	public static function make($db_host, $db_name, $db_user, $db_password)
 	{
 		$dsn = "mysql:host=$db_host;dbname=$db_name;charset=UTF8";
 
 		try {
 			$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
-			return new PDO($dsn, $db_user, $db_pass, $options);
+			return new PDO($dsn, $db_user, $db_password, $options);
 		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
 	}
 }
 
-return Connection::make($db_host, $db_name, $db_user, $db_pass);
+return Connection::make($db_host, $db_name, $db_user, $db_password);
 
 ?>
