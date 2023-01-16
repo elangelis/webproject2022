df048e8f4207f5c3beff09954ea116a33f93ba46
elangelis <elangelis@yahoo.gr>
2023-01-16 11:28:35 +0200

    main

--- MySQL/Functions/PROCEDURES/2/2.a.Show_Shops_User_Location.sql
+++ MySQL/Functions/PROCEDURES/2/2.a.Show_Shops_User_Location.sql
@@ -9,13 +9,20 @@ DECIMAL max_latitude =in_latitude+50;
 DECIMAL min_latitude =in_latitude-50;
 DECIMAL max_longitude=in_longitude+50;
 DECIMAL min_longitude=in_longitude-50;
+DECIMAL absolute_longitude;
+DECIMAL absolute_latitude;
 
 BEGIN
+    absolute_latitude=ABS(min_latitude)+ABS(max_latitude);
+    absolute_longitude=ABS(min_longitude)+ABS(max_longitude);
+
 
     SELECT id,name,address,description,products,has_Offer,latitude,longitude
     FROM Shop
     WHERE (latitude >= min_latitude OR  latitude <= max_latitude)
     AND (longitude <=max_longitude OR  longitude >= min_longitude)
+    AND absolute_latitude<=50
+    AND absolute_longitude<=50;
     AND has_Offer=TRUE;
     
 END &&  
