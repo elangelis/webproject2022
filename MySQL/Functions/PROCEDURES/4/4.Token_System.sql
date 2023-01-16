3cb37f3824cdf3db22c0171158784740edfb9797
elangelis <elangelis@yahoo.gr>
2023-01-16 15:11:09 +0200

    main

--- MySQL/Functions/PROCEDURES/4/4.Token_System.sql
+++ MySQL/Functions/PROCEDURES/4/4.Token_System.sql
@@ -2,30 +2,21 @@ USE webproject2022;
 
 DROP PROCEDURE Token_Bank;
 
+
 DELIMITER &&  
 CREATE PROCEDURE Token_Bank
 DECLARE 
-
-INTEGER UserSum;
-INTEGER Bank_Sum;
-INTEGER Month_Avail;
-
+    INTEGER UserSum;
+    INTEGER Bank_Sum;
+    INTEGER Month_Avail;
 BEGIN
-
-SELECT COUNT(*) INTO UserSum
-FROM User;
-
-Bank_Sum=50*UserSum;
-Month_Avail=Bank_Sum/(80*100);
-
-INSERT INTO TokenBank 
-(Bank_TOTAL,Bank_AVAIL,User_Count,Token_Date) 
-VALUES
-(Bank_Sum,Month_Avail,UserSum,CURRENT_TIMESTAMP);
-
-
+    SELECT COUNT(*) INTO UserSum
+    FROM User;
+    Bank_Sum=50*UserSum;
+    Month_Avail=Bank_Sum/(80*100);
+    INSERT INTO TokenBank(Bank_TOTAL,Bank_AVAIL,User_Count,Token_Date) VALUES (Bank_Sum,Month_Avail,UserSum,CURRENT_TIMESTAMP);
 END&&
-
 DELIMITER;
+
 
 CALL Token_Bank;
---
+++ MySQL/Functions/PROCEDURES/5/5.a.i.Valuation_Token_Day.sql
@@ -0,0 +1,16 @@
+use webproject2022;
+
+DROP PROCEDURE ValuationTokenDay;
+
+DELIMITER&&
+CREATE PROCEDURE ValuationTokenDay
+DECLARE
+
+BEGIN
+
+
+
+END&&
+DELIMITER;
+
+CALL ValuationTokenDay;
