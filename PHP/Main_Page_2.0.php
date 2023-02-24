87ea1336c5a62e6c9cae8f44f0df37347bdba97a
elangelis <73367290+elangelis@users.noreply.github.com>
2023-02-24 20:06:26 +0200

    main

---
+++ PHP/Main_Page_2.0.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css">
+    <link rel="stylesheet" href=PHP/ >
+    <link rel="stylesheet" href="/HTML_CSS_JAVASCRIPT/2.Main_Page/Main_Page.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+    <!--<title>Dashboard Sidebar Menu</title>-->
+</head>
+<body>
+    <!-- Main Page Contents -->
+    <div id="contents">
+        <div id="sidebar_left">
+            <a href="link1.php">
+                <div>
+                    <p>link 1</p>
+                </div>
+            </a>
+        </div>
+        <div id="sidebar_right">
+            <a href="link2.php">
+                <div>
+                    <p>link 2</p>
+                </div>
+            </a>
+        </div>
+        <div id="mainContents">
+            <div class="center-div" style="width: 700px;">
+                <h1">This is some content</h1>
+            </div>
+            <div class="center-div" style="width: 900px;">
+                <h1>More content</h1>
+            </div>
+        </div>
+    </div>
+    <div name='scripts'>
+        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+        <script type="text/javascript" src="/webproject2022/HTML_CSS_JAVASCRIPT/2.Main_Page/Main_Page.js"></script>
+        <script type="text/javascript" src="/webproject2022/HTML_CSS_JAVASCRIPT/2.Main_Page/leaflet.js"></script>
+    </div>
+</body>
+
+</html>
